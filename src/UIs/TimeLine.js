9796b15a224a0f7d679f0fe24cf6c2678e3726e6
SeongJin <jsj0718tjdwl@naver.com>
2020-04-09 13:23:41 +0900

    Added all Time Line things

--- src/UIs/TimeLine.js
+++ src/UIs/TimeLine.js
@@ -8,31 +8,63 @@ const TimeLines = () => {
   return (
     <Div>
       <Timeline>
-        <TimeLine title={"Birth"} titleColor={"#f2a51a"} content={"2000년 7월 18일 출생...."}/>
-        <TimeLine title={"Fuck You"} titleColor={"blue"} content={"Hey Just Fuck Yourself"}/>
+        <TimeLine
+          title={"Birth"}
+          titleColor={"#f2a51a"}
+          content={"2000/ 07/ 18 출생...."}
+        />
+        <TimeLine
+          title={"Middle School"}
+          titleColor={"white"}
+          content={"2013 보인중학교 입학"}
+          bubbleColor ={"#f2ed6f"}
+        />
+        <TimeLine
+          title={"High School"}
+          titleColor={"white"}
+          content={"2016 보인중학교 졸업 & 한세사이버보안고등학교 입학"}
+          bubbleColor ={"#f2ed6f"}
+        />
+        <TimeLine
+          title={"Joined Flysher"}
+          titleColor={"skyblue"}
+          content={"2019 플라이셔 입사 & 한세 사이버 보안고등학교 졸업 & 산업기능요원 선발 "}
+          bubbleColor={"skyblue"}
+        />
+        <TimeLine
+          title={"Now"}
+          titleColor={"#b9ebcc"}
+          content={"2020 산업기능요원 Ing... & React.js Learning!! "}
+          bubbleColor={"green"}
+          now
+        />
       </Timeline>
     </Div>
   );
 };
 
-const TimeLine = (props) =>{
-    return(
-        <TimelineEvent
-          title={props.title || "NONE"}
-          titleStyle={{
-            color: props.titleColor ||"white",
-            "font-size": "50px",
-          }}
-          contentStyle={{
-            color: "black",
-            "font-size": "30px",
-            "background-color": props.contentColor ||"white",
-          }}
-        >
-          {props.content || "Please Enter some Texts"}
-        </TimelineEvent>
-    )
-}
+const TimeLine = (props) => {
+  return (
+    <TimelineEvent
+      title={props.title || "NONE"}
+      titleStyle={{
+        color: props.titleColor || "white",
+        "font-size": "50px",
+        "font-weight" : props.now?"bold":null
+      }}
+      contentStyle={{
+        color: "black",
+        "font-size": "30px",
+        "background-color": props.contentColor || "white",
+      }}
+      bubbleStyle={{
+          "background-color" : props.bubbleColor||"#2c2c2c"
+      }}
+    >
+      {props.content || "Please Enter some Texts"}
+    </TimelineEvent>
+  );
+};
 
 const Div = styled.div`
   width: 100%;
