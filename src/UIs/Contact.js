b0b2bb06b07771b907713b92d1b401e2426f5765
SeongJin <jsj0718tjdwl@naver.com>
2020-04-11 20:19:33 +0900

    Done with Card!!

--- src/UIs/Contact.js
+++ src/UIs/Contact.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Div, Container } from "./Components/Div";
 
-import { FaReact } from "react-icons/fa";
+import { FaReact, FaPhoneVolume } from "react-icons/fa";
+import { MdMail,MdMouse } from "react-icons/md";
+import { GoMarkGithub } from "react-icons/go";
 import { IconContext } from "react-icons";
 
 import ReactCardFlip from "react-card-flip";
@@ -29,9 +31,15 @@ const Contact = () => {
         flipSpeedFrontToBack={0.6}
         flipSpeedBackToFront={0.4}
       >
-        <Card onMouseOver={handleMouseOver}></Card>
+        <Card onMouseOver={handleMouseOver} bgColor="#2c2c2c" front>
+            <MouseIcon>
+                <IconContext.Provider value={{size:"3em",color:"white"}}>
+                    <MdMouse />
+                </IconContext.Provider>
+            </MouseIcon>
+        </Card>
         <Card bgColor="white">
-          <IconContext.Provider value={{ size: "5em", color: "#084177" }}>
+          <IconContext.Provider value={{ size: "3em", color: "#084177" }}>
             <FaReact
               style={{
                 position: "absolute",
@@ -46,7 +54,13 @@ const Contact = () => {
             <NameH1>Seongjin Jeong</NameH1>
             <NameH3>React Developer</NameH3>
           </Name>
-          <Contacts></Contacts>
+          <Contacts>
+              <IconContext.Provider value={{size:"1em",color:"black"}}>
+              <Items><FaPhoneVolume /> +82.10.9583.7412</Items>
+              <Items><MdMail /> &nbsp;&nbsp;jsj0718tjdwl@naver.com</Items>
+              <Items><GoMarkGithub /> &nbsp;&nbsp;<a href="https://github.com/SeongJinJeong" target="_blank">https://github.com/SeongJinJeong</a></Items>
+              </IconContext.Provider>
+          </Contacts>
         </Card>
       </ReactCardFlip>
     </Div>
@@ -58,6 +72,7 @@ const Card = styled.div`
   height: 30vh;
 
   position: relative;
+//   ${props=>props.front?`display:flex`:null}
 
   background-color: ${(props) => props.bgColor || "palevioletred"};
 
@@ -81,7 +96,7 @@ const NameH1 = styled.h1`
 `;
 
 const NameH3 = styled.h3`
-  font-weight: 300px;
+  font-weight: 300;
   margin-top: 0px;
   margin-left: 5px;
 `;
@@ -89,7 +104,36 @@ const NameH3 = styled.h3`
 const Contacts = styled.div`
   float: right;
   margin-right: 50px;
-  margin-bottom: 30px;
+  margin-bottom: 10px;
+`;
+
+const Items = styled.p`
+    border-left : 3px solid blue;
+    padding : 10px;
+    margin:0px;
+`;
+
+const HoverKey = keyframes`
+    0% {
+        right:45%;
+        bottom : 0%;
+    }
+    20%{
+        right:45%;
+        bottom:45%;
+    }
+    100%{
+        right:45%;
+        bottom:45%;
+    }
+`;
+
+const MouseIcon = styled.button`
+    position : absolute;
+    animation: ${HoverKey} 1.5s alternate infinite;
+    animation-timing-function : ease-in-out;
+    background-color : #2c2c2c;
+    border : none;
 `;
 
 export default Contact;
